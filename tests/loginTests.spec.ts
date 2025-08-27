9ea1e04797d2d40d1ac598b06d6ae48f94f39f65
Dmytro Spitchenko <dmytro.spitchenko@dataart.com>
2025-08-27 16:55:30 +0200

    aug-21-added new json file with users test data

--- tests/loginTests.spec.ts
+++ tests/loginTests.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
+import users from '../test-data/users.json';
 
 let loginPage: LoginPage;
 
@@ -10,7 +11,7 @@ test.beforeEach(async ({page})=>{
 });
 
 test("User can successfully login to the main page with valid credentials", async ({ page }) => {
-    const inventoryPage: InventoryPage = await loginPage.logIn("standard_user", "secret_sauce");
+    const inventoryPage: InventoryPage = await loginPage.logIn(users.standardUser.username, users.standardUser.password);
     await expect(inventoryPage.spanProducts).toHaveText("Products");
 })
 
--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
+import users from '../test-data/users.json';
 
 let inventoryPage: InventoryPage;
 
 test.beforeEach(async ({page})=>{
     const loginPage: LoginPage = new LoginPage(page);
     await loginPage.goToLoginPage();
-    inventoryPage = await loginPage.logIn("standard_user", "secret_sauce");
+    inventoryPage = await loginPage.logIn(users.standardUser.username, users.standardUser.password);
+    
 });
 
 test("Products should be present on Inventory page", async({page})=>{
