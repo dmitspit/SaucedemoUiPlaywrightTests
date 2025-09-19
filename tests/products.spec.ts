917dc1ea7444c4b7b6a1593df8c1853cace4defa
Dmytro Spitchenko <dmytro.spitchenko@dataart.com>
2025-09-19 12:35:05 +0200

    Added playwright fixtures

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -1,44 +1,43 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-import { InventoryPage } from '../pages/InventoryPage';
-import users from '../test-data/users.json';
-import { ProductModel } from '../models/ProductModel';
+import { test, expect } from "../fixtures/test-options";
+import { ProductModel } from "../models/ProductModel";
 import { ProductSort } from "../enums/ProductSort";
 
-let inventoryPage: InventoryPage;
+test.describe("Inventory Page Tests", () => {
+  test("Products should be present on Inventory page", async ({
+    page,
+    inventoryPage,
+  }) => {
+    const products: ProductModel[] = await inventoryPage.getAllProducts();
+    expect(products).toHaveLength(6);
 
-test.describe('Inventory Page Tests', () => {
-    test.beforeEach(async ({page})=>{
-        const loginPage: LoginPage = new LoginPage(page);
-        await loginPage.goToLoginPage();
-        inventoryPage = await loginPage.logIn(users.standardUser.username, users.standardUser.password);
-    });
+    for (const product of products) {
+      expect(product.name).toBeDefined();
+      expect(product.description).toBeDefined();
+      expect(product.price).toBeDefined();
 
-    test("Products should be present on Inventory page", async({page})=>{
-        const products: ProductModel[] = await inventoryPage.getAllProducts();
-        expect(products).toHaveLength(6);
+      await expect(product.addToCartBtn).toBeVisible();
+      await expect(product.image).toBeVisible();
+    }
+  });
 
-        for (const product of products) {
-            expect(product.name).toBeDefined();
-            expect(product.description).toBeDefined();
-            expect(product.price).toBeDefined();
+  test("User can sort products by price (low to high)", async ({
+    page,
+    inventoryPage,
+  }) => {
+    const products: ProductModel[] = await inventoryPage.getAllProducts();
 
-            await expect(product.addToCartBtn).toBeVisible();
-            await expect(product.image).toBeVisible();
-        }
-    });
+    expect(products).toHaveLength(6);
 
-    test("User can sort products by price (low to high)", async ({ page }) => {
-        const products: ProductModel[] = await inventoryPage.getAllProducts();
+    const minPrice: number = Math.min(
+      ...products.map((p) => parseFloat(p.price))
+    );
+    await inventoryPage.sortProducts(ProductSort.PRICE_LOW_HIGH);
+    const firstItem: number = (await inventoryPage.getAllProducts()).map((p) =>
+      parseFloat(p.price)
+    )[0];
 
-        expect(products).toHaveLength(6);
+    await page.screenshot({ path: "screenshots/sorted.png" });
 
-        const minPrice: number = Math.min(... products.map(p=>parseFloat(p.price)));
-        await inventoryPage.sortProducts(ProductSort.PRICE_LOW_HIGH);
-        const firstItem: number = (await inventoryPage.getAllProducts()).map(p=>parseFloat(p.price))[0];
-        
-        await page.screenshot({path: 'screenshots/sorted.png'});
-        
-        expect(minPrice).toBe(firstItem);
-    });
+    expect(minPrice).toBe(firstItem);
+  });
 });
